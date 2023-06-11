2011eaad587cb1d058aed4bbf7b389603f7519bd
nurulamin <admin@nurulamindig.com>
2023-06-11 19:49:12 +0600

    this is break and continue

--- break-continue.js
+++ break-continue.js
@@ -1,19 +1,19 @@
 console.log("this is break");
 
-/*const num=[1,2,3,4,5,6,7];
+const num=[1,2,3,4,5,6,7];
 for(let i=0; i<num.length;i++){
     console.log(num[i])
 }
-*/
+
 // it is normal role
-/*const num=[1,2,3,4,5,6,7,8,9];
+const num=[1,2,3,4,5,6,7,8,9];
 for(let i=0; i<num.length;i++){
 
     if(num[i]>6){
         break;
     }
     console.log(num[i]);
-};*/ //it is break end of 6 arroy
+}; //it is break end of 6 arroy
 
 const num=[-1,-2,-3,-4,5,6,7];
 for(let i=0; i<num.length;i++){
@@ -23,4 +23,4 @@ for(let i=0; i<num.length;i++){
 
     console.log(num[i])
 }
-// continue means that it ignore - num in this arroy by contition
+// continue means that it ignore - num in this arroy by contition.
